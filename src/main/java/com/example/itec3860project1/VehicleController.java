dedec5d15f3efdf5334a4d7cf4a3d6d330bbfdbc
Steven Lopez <slopez@rufuslabs.com>
2024-09-10 19:36:36 -0400

    Added update and delete methods

--- src/main/java/com/example/itec3860project1/VehicleController.java
+++ src/main/java/com/example/itec3860project1/VehicleController.java
@@ -3,6 +3,8 @@ package com.example.itec3860project1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
@@ -35,19 +37,56 @@ public class VehicleController {
 
     @RequestMapping(value = "/updateVehicle", method = RequestMethod.PUT)
     public Vehicle updateVehicle(@RequestBody Vehicle newVehicle) throws IOException {
+
+        boolean isMatched = false;
+
         List<String> lines = FileUtils.readLines(new File("./vehicles.txt"), "UTF-8");
-        for (String line : lines) {
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
             Vehicle v = mapper.readValue(line, Vehicle.class);
             if (v.getId() == newVehicle.getId()) {
+                v = newVehicle;
+                lines.set(i, mapper.writeValueAsString(v));
                 FileUtils.writeStringToFile(new File("./vehicles.txt"), "","UTF-8", false);
+                isMatched = true;
             }
         }
 
-        for (String line : lines) {
-            FileUtils.writeStringToFile(new File("./vehicles.txt"), line + "\n","UTF-8", true);
+        if (isMatched) {
+            for (String line : lines) {
+                FileUtils.writeStringToFile(new File("./vehicles.txt"), line + "\n","UTF-8", true);
+            }
+
+            return newVehicle;
+        } else {
+            return new Vehicle(0, "", 0,0);
         }
-        return null;
     }
 
+    @RequestMapping(value = "/deleteVehicle/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteVehicle(@PathVariable int id) throws IOException {
+        boolean isMatched = false;
 
+        List<String> lines = FileUtils.readLines(new File("./vehicles.txt"), "UTF-8");
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+
+            Vehicle vehicle = mapper.readValue(line, Vehicle.class);
+
+            if (vehicle.getId() == id) {
+                lines.remove(i);
+                FileUtils.writeStringToFile(new File("./vehicles.txt"), "","UTF-8", false);
+                isMatched = true;
+            }
+        }
+
+        if (isMatched) {
+             for (String line : lines) {
+                 FileUtils.writeStringToFile(new File("./vehicles.txt"), line + "\n","UTF-8", true);
+             }
+             return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Delete failed", HttpStatus.NOT_FOUND);
+        }
+    }
 }
