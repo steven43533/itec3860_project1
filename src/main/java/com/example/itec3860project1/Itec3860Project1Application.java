63700cc28cc159f17a2663c016a33b7bcc3cf368
Steven Lopez <slopez@rufuslabs.com>
2024-09-10 19:08:13 -0400

    first commit

---
+++ src/main/java/com/example/itec3860project1/Itec3860Project1Application.java
@@ -0,0 +1,13 @@
+package com.example.itec3860project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itec3860Project1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itec3860Project1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itec3860project1/Itec3860Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itec3860project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itec3860Project1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
